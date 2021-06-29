246b4dd01adba8fe845114dce29c65be6bc03489
dcastano11 <castanodavid@gmail.com>
2021-06-29 17:06:56 -0500

    init

--- ios/Classes/SwiftSoundGeneratorPlugin.swift
+++ ios/Classes/SwiftSoundGeneratorPlugin.swift
@@ -27,7 +27,7 @@ public class SwiftSoundGeneratorPlugin: NSObject, FlutterPlugin {
     super.init()
     //self.mixer = Mixer()
     //self.mixer.init()
-    self.oscillator.init()
+    self.oscillator.init(waveform:Table(.triangle))
     self.mixer.addInput(oscillator)
     self.mixer.volume = 1.0
     Settings.disableAVAudioSessionCategoryManagement = true
